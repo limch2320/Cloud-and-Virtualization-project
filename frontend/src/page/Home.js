47ade668184ff7f13e9dbbf3ef2148a5e6e373b3
limch2320 <limch2320@naver.com>
2025-03-14 22:21:36 +0900

    bugfix1.2

---
+++ frontend/src/page/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>홈</h1>
+      <div className="home-content">
+        <p>웹사이트에 오신 것을 환영합니다.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
